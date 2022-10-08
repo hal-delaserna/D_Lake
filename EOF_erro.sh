7c30a277d3048e2ced8b5fec37d7c32059850c5c
humberto almeida de la serna <52361218+hal-delaserna@users.noreply.github.com>
2022-10-08 19:55:09 -0300

    Add files via upload

---
+++ EOF_erro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash'
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y0.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y1.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y2.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y3.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y4.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y5.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y6.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y7.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y8.D20709.ESTABELE
+sed -i {'s/[[:alpha:]]//g ; s/[[:blank:]]//g ; s/@/ at /g; s/";"/@/g; s/^"/#/g; s/"$/#/g;  s/"//g; s/@/";"/g;  s/#/"/g;  '}  K3241.K03200Y9.D20709.ESTABELE
